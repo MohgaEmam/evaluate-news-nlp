270345f920a052baa11f56e98a2e0906fab9afeb
MohgaEmam <38896263+MohgaEmam@users.noreply.github.com>
2020-05-09 08:22:49 +0200

    Update postHandler.js

--- src/server/postHandler.js
+++ src/server/postHandler.js
@@ -9,23 +9,52 @@ function inputValidation(req, res, next) {
   return next();
 }
 
-function handlePost(req, res, next) {
-  var aylien = require("aylien_textapi");
-  // Aylien API credentias load app_id and app_key from .env file
-  // Please make sure to carate an account in aylian and obtain your own app_id and app_key
-  // then create a .env file and set it.
-  var textapi = new aylien({
+// function handlePost(req, res, next) {
+//   var aylien = require("aylien_textapi");
+//   // Aylien API credentias load app_id and app_key from .env file
+//   // Please make sure to carate an account in aylian and obtain your own app_id and app_key
+//   // then create a .env file and set it.
+//   var textapi = new aylien({
+//     application_id: process.env.API_ID,
+//     application_key: process.env.API_KEY,
+//   });
+//   textapi.sentiment(
+//     {
+//       url: req.body.text,
+//     },
+//     function (error, response) {
+//       res.send(response);
+//     }
+//   );
+// }
+// exports.inputValidation = inputValidation;
+// exports.handlePost = handlePost;
+
+var textapi = new aylien({
     application_id: process.env.API_ID,
-    application_key: process.env.API_KEY,
-  });
-  textapi.sentiment(
-    {
-      url: req.body.text,
-    },
-    function (error, response) {
-      res.send(response);
-    }
-  );
+    application_key: process.env.API_KEY
+});
+
+function getArticle(req, res) {
+    textapi.sentiment({
+            url: req.body.url
+        },
+        function(error, response) {
+            res.send(response);
+        }
+    );
 }
-exports.inputValidation = inputValidation;
-exports.handlePost = handlePost;
+
+function getText(req, res) {
+    textapi.sentiment({
+            text: req.body.url
+        },
+        function(error, response) {
+            res.send(response);
+        }
+    );
+}
+
+
+exports.getArticle = getArticle;
+exports.getText = getText;
